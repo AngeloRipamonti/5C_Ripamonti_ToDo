02c27d79c94f8d624516f5236780c816a11187f2
AngeloRipamonti <ripamontiangelo@itis-molinari.eu>
2025-02-01 17:35:40 +0100

    implemented database

--- index.js
+++ index.js
@@ -3,30 +3,84 @@ const express = require("express");
 const bodyParser = require('body-parser');
 let http = require("http");
 const path = require('path');
+const fs = require('fs');
+const mysql = require('mysql2');
+const config = JSON.parse(fs.readFileSync('config.json'));
+config.ssl.ca = fs.readFileSync(__dirname + '/ca.pem');
+const connection = mysql.createConnection(config);
 const app = express();
-let todos = [];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname, "public")));
 
-app.post("/todo/add", (req, res) => {
+function executeQuery(sql) {
+   return new Promise((resolve, reject) => {      
+      connection.query(sql, function (err, result) {
+         if (err) {
+            console.error(err);
+            reject(err);     
+         }   
+         resolve(result);         
+   });
+})
+}
+
+async function createTable() {
+   return await executeQuery(`
+      CREATE TABLE IF NOT EXISTS todo ( 
+         id INT PRIMARY KEY AUTO_INCREMENT, 
+         name VARCHAR(255) NOT NULL, 
+         completed BOOLEAN 
+      );
+   `);
+}
+
+async function insert(todo) {
+   const template = `INSERT INTO todo (name, completed) VALUES ('$NAME', '$COMPLETED');`;
+   let sql = template.replace("$NAME", todo.name);
+   sql = sql.replace("$COMPLETED", todo.completed ? 1 : 0);
+   return await executeQuery(sql);
+}
+async function update(todo) {
+   const template = `UPDATE todo SET completed = $COMPLETED WHERE id = $ID;`;
+   let sql = template.replace("$ID", todo.id);
+   sql = sql.replace("$COMPLETED", todo.completed ? 1 : 0);
+   return await executeQuery(sql);
+
+}
+
+async function deleteSQL(todo) {
+   const template = `DELETE FROM todo WHERE id = $ID;`;
+   let sql = template.replace("$ID", todo.id);
+   return await executeQuery(sql);
+}
+
+async function select() {
+   const sql = `SELECT id, name, completed FROM todo;`;
+   return await executeQuery(sql);
+}
+
+
+app.post("/todo/add", async (req, res) => {
    const todo = req.body.todo;
-   todo.id = "" + new Date().getTime();
-   todos.push(todo);
+   //todo.id = "" + new Date().getTime();
+   await insert(todo);
    res.json({ result: "Ok" });
 });
 
-app.get("/todo", (req, res) => {
+app.get("/todo", async (req, res) => {
+   const todos = await select();
    res.json({ todos: todos });
 });
 
-app.put("/todo/complete", (req, res) => {
+app.put("/todo/complete", async (req, res) => {
    const todo = req.body;
    try {
-      todos = todos.map((element) => {
+      await (await select()).map(async (element) => {
          if (element.id === todo.id) {
             element.completed = true;
+            await update(element);
          }
          return element;
       })
@@ -36,14 +90,17 @@ app.put("/todo/complete", (req, res) => {
    res.json({ result: "Ok" });
 });
 
-app.delete("/todo/:id", (req, res) => {
-   todos = todos.filter((element) => element.id !== req.params.id);
+app.delete("/todo/:id", async (req, res) => {
+   const todos = await select();
+   const toDelete = todos.filter((element) => element.id == req.params.id);
+   await deleteSQL(toDelete[0]);
    res.json({ result: "Ok" });
 })
 
 const server = http.createServer(app);
 
+createTable();
+
 server.listen(80, () => {
-   console.log(server.address());
    console.log("- server running");
 });
